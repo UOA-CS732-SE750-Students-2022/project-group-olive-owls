bff0f38a14240af0abc52c1a7fd73ebb11dd3f6c
Kieran-byte <bettesworthkieran1@gmail.com>
2022-05-10 13:34:35 +1200

    Added BubbleEdit function, link to it in App.js and so small reformatting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,22 @@
 import './App.css';
 
-import SignIn from './component/signIn';
-import Register from './component/register';
 
-import Alert from './component/Alert';
-import Root from './component/root';
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-
-
-
 import {
     BrowserRouter as Router,
     Routes,
     Route
 } from "react-router-dom";
+//imports of components
+import SignIn from './component/signIn';
+import Register from './component/register';
+import Alert from './component/Alert';
+import Root from './component/root';
 import {DragAndDrop} from "./component/DragAndDrop";
 import SideBar from "./component/Sidebar";
 import Records from './component/Records'
+import BubbleEdit from "./component/BubbleEdit";
 
 function App() {
     return (
@@ -30,6 +29,10 @@ function App() {
                         <Route path="/" element={<Root />}>
 
                         </Route>
+
+                        //adds a new route for the bubble edit
+                        <Route path={"/BubbleEdit"} element={<BubbleEdit />}></Route>
+
                         <Route path="/register" element={<Register />}>
 
                         </Route>
---
+++ frontend/src/component/BubbleEdit.js
@@ -0,0 +1,13 @@
+import Container from "@mui/material/Container";
+import {Box} from "@mui/material";
+import {Link} from "react-router-dom";
+import Button from "@mui/material/Button";
+import * as React from "react";
+
+export default function BubbleEdit() {
+
+    return (
+        //needs to query the databse anout what the bubble holds and then be able to remove or assing employees to it
+    <h1>JavaScript Display Text</h1>
+    )
+}
